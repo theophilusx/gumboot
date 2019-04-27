f4d6d48eeecfd8464b5635fa955ccf502ec062bc
Tim Cross <theophilusx@gmail.com>
2019-04-27 13:46:39 +1000

    Add collapsable cards

--- src/gumboot/card.cljs
+++ src/gumboot/card.cljs
@@ -19,3 +19,25 @@
      [:div {:class (str "card-footer " footer-class)}
       footer])])
 
+(defn card-collapsable
+  [id header body & {:keys [footer title card-class header-class footer-class]
+                     :or {footer nil
+                          title nil
+                          card-class ""
+                          header-class ""
+                          footer-class ""}}]
+  [:div {:class (str "card " card-class)}
+   [:div {:class (str "card-header " header-class)}
+    [:h2.mb-0
+     [:button.btn.btn-link {:type "button"
+                            :data-toggle "collapse"
+                            :data-target (str "#" id)}
+      header]]]
+   [:div {:class "collapse" :id id}
+    [:div.card-body
+     (if (not (nil? title))
+       [:h5.card-title title])
+     body]
+    (if (not (nil? footer))
+      [:div {:class (str "card-footer " footer-class)}
+       footer])]])
